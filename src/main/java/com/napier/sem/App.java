8dbca81c4a1de757406c46c03c3a8684db5678ac
zanica <zanica3.0@hotmail.com>
2025-10-30 21:34:03 -0400

    Updated App.java.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,7 @@
 package com.napier.sem;
 
 public class App {
-    public static void main(String[] args)
+    static void main(String[] ignoredArgs)
     {
         System.out.println("Boo yah!");
     }
